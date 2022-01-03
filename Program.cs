a10efd129736ef011530aecea961dd3f52b0f906
SergeyPascko <92270075+SergeyPascko@users.noreply.github.com>
2022-01-03 03:45:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+ 
+class Program
+{
+    static void Main()
+    {
+        int[] test = CopyEven(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        foreach (int num in test)
+        {
+            Console.Write(num + ",");
+        }
+        Console.WriteLine();
+    }
+ 
+    static int[] CopyEven(int[] numbers)
+    {
+        int[] result = new int[0];
+ 
+        foreach (int element in numbers)
+        {
+            if ((element % 2 == 0) && (element != 0))
+            {
+                Array.Resize(ref result, result.Length + 1);
+                result[result.Length - 1] = element;
+            }
+        }
+        return result;
+    }
+}
